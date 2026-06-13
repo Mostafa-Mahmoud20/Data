c9d8982b92883a3a3a97a67e2b5545c4454d2f0f
mostafa <mm5981@fayoum.edu.eg>
2026-06-13 05:00:11 +0300

    Edit 4

--- api/prediction.py
+++ api/prediction.py
@@ -5,14 +5,15 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# CORS
+from fastapi.middleware.cors import CORSMiddleware
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PRED_DIR = os.path.join(BASE_DIR, "Predictions")
