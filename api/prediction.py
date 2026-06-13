203baae66d03dd0e745162abe8492be7f0399c2e
mostafa <mm5981@fayoum.edu.eg>
2026-06-13 04:58:33 +0300

    Edit 3

--- api/prediction.py
+++ api/prediction.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # For testing. Later replace with your frontend URL.
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
